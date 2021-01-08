4d5217d0fb8c937ca138b61993456bc166c94cbb
Joon Sik Yang <joonsikyang@Joons-MacBook-Pro.local>
2021-01-08 15:04:05 +0900

    Add : Mock 데이터를 활용한 화면 구현(본죽 예시)

---
+++ src/pages/Main/commentData.js
@@ -0,0 +1,38 @@
+export const commentData = [
+  {
+    id: 1,
+    username: '1username',
+    content: '첫 번째 comment',
+    isLiked: false
+  },
+  {
+    id: 2,
+    username: '2username',
+    content: '두 번째 comment',
+    isLiked: false
+  },
+  {
+    id: 3,
+    username: '3username',
+    content: '세 번째 comment',
+    isLiked: false
+  },
+  {
+    id: 4,
+    username: '4username',
+    content: '네 번째 comment',
+    isLiked: false
+  },
+  {
+    id: 5,
+    username: '5username',
+    content: '다섯 번째 comment',
+    isLiked: false
+  },
+  {
+    id: 6,
+    username: '6username',
+    content: '여섯 번째 comment',
+    isLiked: false
+  }
+];
